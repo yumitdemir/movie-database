5f85178bb2396b6ef3e4b700fd0f4d1b6c9e0469
yumitdemir <yumitdemir@outlook.com>
2025-04-09 00:15:02 +0300

    better

--- routes/web.php
+++ routes/web.php
@@ -45,17 +45,5 @@ Route::post('/media', [MediaController::class, 'store'])->name('media.store');
 Route::put('/media/{id}', [MediaController::class, 'update'])->name('media.update');
 Route::delete('/media/{id}', [MediaController::class, 'destroy'])->name('media.destroy');
 
-// Authentication Routes
-Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
-Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
-
-// Registration Routes
-Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
-
-// Password Reset Routes
-Route::get('/password/reset', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
-Route::post('/password/email', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::get('/password/reset/{token}', [App\Http\Controllers\Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('/password/reset', [App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
+// Include Auth Routes
+require __DIR__.'/auth.php';
